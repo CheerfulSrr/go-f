baf7fb19c858f153d49daf4191f50f3987188e7c
greekn <cheerfuls@outlook.com>
2020-11-06 23:30:56 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { render } from 'react-dom';
+
+render(
+  1,
+  document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,127 @@
+'use strict'
+const path = require('path');
+const { argv } = require('yargs')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const backendPath = 'http://localhost:8080'
+
+const {
+  mode = 'development'
+} = argv
+
+module.exports = {
+  mode: mode === 'development' ? mode : 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'js/[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
+  },
+  optimization: {
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: 'dist',
+    open: true,
+    hot: true,
+    port: 5000,
+    // proxy: [
+    //   {
+    //     context: [
+    //       "/"
+    //     ],
+    //     target: backendPath + '/'
+    //   },
+    //   {
+    //     context: path => {
+    //       return /^\/resources\/js\/bundle\.(css|js)$/.test(path);
+    //     },
+    //     target: backendPath + '/',
+    //     changeOrigin: true,
+    //   }
+    // ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      }
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new MiniCssExtractPlugin({
+      filename: 'static/css/[name].bundle.css',
+      chunkFilename: 'static/css/[name].bundle.css'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+    extensions: [ '.js', '.jsx', '.json' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader' ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                strictMath: true,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ],
+  }
+}
